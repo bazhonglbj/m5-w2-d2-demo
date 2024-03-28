21856e4c07c3578e8bebc9457fed838be1060199
bazhonglbj <bazhonglbj@gmail.com>
2024-03-27 21:30:19 -0700

    m5w2d2-demo

---
+++ demo-app/src/routerexample1.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+
+  function Home() {
+    return (
+      <div>
+        <h2>Home</h2>
+        <p>This is home</p>
+      </div>
+    );
+  }
+  
+  function About() {
+    return <h2>About</h2>;
+  }
+  
+  function Users() {
+    return <h2>Users</h2>;
+  }
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
